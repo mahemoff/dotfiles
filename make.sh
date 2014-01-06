3056136533c01cde86d7ad22f6ded5dda8ef5057
Mahemoff <michael@mahemoff.com>
2014-01-06 18:18:34 +0000

    fix make.sh

--- make.sh
+++ make.sh
@@ -26,7 +26,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
   echo "Moving any existing dotfiles from ~ to $backupdir"
-  mv ~/.$file ~/dotfiles_old/
+  mv ~/.$file $backupdir
   echo "Creating symlink to $file in home directory."
   ln -s $dir/$file ~/.$file
 done
