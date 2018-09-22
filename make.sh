8cd5710ff257c5d3a8600b0981160edabdab2bf5
Michael Mahemoff <michael@mahemoff.com>
2018-09-22 09:32:07 +0100

    add toprc + optional logging in make.sh
    [ci skip]

--- make.sh
+++ make.sh
@@ -5,32 +5,46 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
+############################
+# HELPER FUNCTIONS
+############################
+
+function log() {
+  if [ "$DOTFILES_LOG" == 'verbose' ]; then
+    echo "$*"
+  fi
+}
+
+############################
+# SCRIPT
+############################
+
 cd `dirname $0`
 
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
 backupdir=~/backup/dotfiles          # old dotfiles backup directory
-files="bash_profile vimrc tmux.conf vim inputrc editrc bash_before bash_after bash_tmux myclirc tigrc irbrc"    # list of (dot)files to symlink in homedir
+files="bash_profile vimrc tmux.conf vim inputrc editrc bash_before bash_after bash_tmux myclirc tigrc irbrc toprc"    # list of (dot)files to symlink in homedir
 
 ##########
 
 # create dotfiles_old in homedir
-echo "Creating $backupdir for backup of any existing dotfiles in ~"
+log "Creating $backupdir for backup of any existing dotfiles in ~"
 mkdir -p $backupdir
-echo "...done"
+log "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
+log "Changing to the $dir directory"
 cd $dir
-echo "...done"
+log "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 
 for file in $files; do
-  echo "Moving any existing dotfiles from ~ to $backupdir"
+  log "Moving any existing dotfiles from ~ to $backupdir"
   if [ -f "$backupdir/.$file" ] ; then rm $backupdir/.$file ; fi # suppress silly "identical file error" from mv
   mv ~/.$file $backupdir 2>/dev/null
-  echo "Creating symlink to $file in home directory."
+  log "Creating symlink to $file in home directory."
   ln -sf $dir/$file ~/.$file
 done
